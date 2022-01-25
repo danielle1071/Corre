0db3dfba2ae8f4f5739699d78aad6746fbca4b7f
Lauren Wright <laurennnwrighttt@gmail.com>
2022-01-24 19:46:18 -0500

    Login page UI added

--- corre/corre/LoginView.swift
+++ corre/corre/LoginView.swift
@@ -10,6 +10,13 @@
 // import Foundation
 import SwiftUI
 
+struct CustomColor {
+    static let backcolor =
+        Color("backgroundColor")
+    
+    static let primarycolor = Color("primaryColor")
+}
+
 struct LoginView: View {
     
     
@@ -21,26 +28,60 @@ struct LoginView: View {
     
     var body: some View {
         VStack {
+            
+            Image("CreamLogo")
+                .resizable()
+                .frame(width: 200.0, height: 200.0)
+                .scaledToFit()
+            
             Spacer()
+                .frame(height: 50)
+            
+            Text("Welcome Back")
+                .font(.system(size: 36.0))
+                .foregroundColor(CustomColor.primarycolor)
+            
+            Spacer()
+                .frame(height: 25)
+                
             
             TextField("Username", text: $login)
+                .textFieldStyle(RoundedBorderTextFieldStyle())
+                .frame(height: 40)
+                .padding([.horizontal], 20)
+                .cornerRadius(16)
+                .shadow(radius: 2.0)
+    
+            
             SecureField("Password", text: $password)
+                .textFieldStyle(RoundedBorderTextFieldStyle())
+                .frame(height: 40)
+                .padding([.horizontal], 20)
+                .cornerRadius(16)
+                .shadow(radius: 2.0)
+                
+            
+            Spacer()
+                .frame(height: 25)
             Button("Login", action: {
                 sessionManager.login(
                     username: login,
                     password: password
                 )
-                // print("Test")
-            })
+            }).padding()
+                .foregroundColor(.white)
+                .padding(.horizontal, 100)
+                .background(CustomColor.primarycolor)
+                .cornerRadius(20)
+                
             
             Spacer()
-            Button("Not a user yet?, sign up!", action:{
+            Button("Don't have an account? Sign up", action:{
                 
                 sessionManager.showSignUp()
                 })
                     
-        }
-        .padding()
+        } .background(CustomColor.backcolor.edgesIgnoringSafeArea(.all))
     }
     
 }
