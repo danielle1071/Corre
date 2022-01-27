6dcf7c42fc18855fc5590256c45c6cb511df96b0
Lauren Wright <laurennnwrighttt@gmail.com>
2022-01-26 20:39:58 -0500

    Added registration and confirmation UI and added all colors to the assets folder.

--- corre/corre/ConfirmationView.swift
+++ corre/corre/ConfirmationView.swift
@@ -4,6 +4,11 @@
 //
 //  Created by Lucas Morehouse on 11/15/21.
 //
+//  Edited by Lauren Wright on 1/26/2022
+//  Added UI
+//  Need to figure out if this is a pop up or
+//  full screen
+//
 //  This file is based on the youtube tutorial: https://www.youtube.com/watch?v=wSHnmtnzbfs
 //
 
@@ -21,29 +26,47 @@ struct ConfirmationView: View {
     let username: String
     
     var body: some View {
+        
         VStack {
             Text("Username: \(username)")
+                .font(.system(size: 23.0))
+                .foregroundColor(CustomColor.primarycolor)
+            
             TextField("Confirmation Code", text:
                         $confirmationCode)
+                .textFieldStyle(RoundedBorderTextFieldStyle())
+                .frame(height: 40)
+                .padding([.horizontal], 20)
+                .cornerRadius(16)
+                .shadow(radius: 2.0)
+            
             Button("Confirm", action: {
                 sessionManager.confirm(
                     username: username,
                     code: confirmationCode
                 )
             })
+                .padding()
+                .foregroundColor(.white)
+                .padding(.horizontal, 100)
+                .background(CustomColor.primarycolor)
+                .cornerRadius(20)
             .padding()
-            Button("Resend code", action: {
+            Button("Didn't get it? Retry", action: {
                     Amplify.Auth.resendSignUpCode(for: username)
                     })
             .padding()
             }
+            .background(CustomColor.backcolor.edgesIgnoringSafeArea(.all))
+            
         }
     
 }
 
+
 struct ConfirmationView_Previews: PreviewProvider {
     static var previews: some View {
-        ConfirmationView(username: "Corre Database Developer!")
+        ConfirmationView(username: "Database Developer")
         
     }
 }
