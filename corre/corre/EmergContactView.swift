10f08940e6c15b2b5545d04edb1ea47dda80727d
Lucas <lucas.morehouse@hotmail.com>
2022-02-14 21:40:19 -0500

    Misc. front end pages

--- corre/corre/EmergContactView.swift
+++ corre/corre/EmergContactView.swift
@@ -26,58 +26,128 @@ import AmplifyMapLibreUI
 struct EmergContactView: View {
     
     @EnvironmentObject var sessionManager: SessionManger
-    @StateObject var locationService = LocationManager()
-    @State var tokens: Set<AnyCancellable> = .init()
-    @State var mapState = AMLMapViewState()
+//<<<<<<< HEAD
+//    @StateObject var locationService = LocationManager()
+//    @State var tokens: Set<AnyCancellable> = .init()
+//    @State var mapState = AMLMapViewState()
+//
+//    let user: AuthUser
+//
+//    var body: some View {
+//
+//        VStack {
+//
+//            // MARK: change this to stop run!
+//            Button("Back", action: {
+//                sessionManager.showSession()
+//
+//                // stops tracker resources
+//                locationService.stopTracking()
+//            })
+//
+//=======
     
-    let user: AuthUser
-    
-    var body: some View {
-        
-        VStack {
-            
-            // MARK: change this to stop run!
-            Button("Back", action: {
-                sessionManager.showSession()
-                
-                // stops tracker resources
-                locationService.stopTracking()
-            })
-            
-            Spacer()
-            
-            AMLMapView(mapState: mapState)
-                .showUserLocation(true)
-                .edgesIgnoringSafeArea(.all)
-            
-            
-            Spacer()
-            Text("Tracking view")
-            Spacer()
-            
-            Button("Sign Out", action: {
-                sessionManager.signOut()
-            })
-        }
-        .onAppear(perform: getRunnerLocation)
-    }
-    
-    // MARK: deleteThis
-    // User's Coordinates:  CLLocationCoordinate2D(latitude: 35.7020691, longitude: 139.7753269)
-    // locationManager - got locations: [<+35.70206910,+139.77532690> +/- 5.00m (speed -1.00 mps / course -1.00) @ 2/9/22, 1:27:34 AM Eastern Standard Time]
+    struct CusColor {
+        static let backcolor =
+            Color("backgroundColor")
 
-    func getRunnerLocation() {
-        
-        locationService.storeUsername(id: "Database")
-        
-        locationService.coordinatesPublisher
-            .receive(on: DispatchQueue.main)
-            .sink { coordinates in
-                print("getCurrentUserLocation - user's Coordinates: ", coordinates)
-                self.mapState.center = coordinates
+        static let primarycolor = Color("primaryColor")
+
+        static let lblue = Color("lightBlue")
+    }
+
+    var body: some View {
+        VStack{
+            Button (action: {sessionManager.showSession()}){
+            HStack{
+                Image(systemName: "arrow.left")
+                    .foregroundColor(Color("primaryColor"))
+                Text("Emergency Contacts")
+                    .padding(/*@START_MENU_TOKEN@*/.horizontal/*@END_MENU_TOKEN@*/)
+                    .foregroundColor(Color("primaryColor"))
+                Spacer()
             }
-            .store(in: &tokens)
-        
+            .padding(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
+            }
+
+
+
+
+            Image("CreamLogo")
+            .resizable()
+            .frame(width: 125.0, height: 125.0)
+            .scaledToFit()
+
+            Button (action: {}){
+                HStack{
+                    Text("Jane Doe")
+                        .foregroundColor(Color("primaryColor"))
+                    Spacer()
+                    Image(systemName: "arrow.right")
+                        .foregroundColor(Color("primaryColor"))
+                }
+                .padding(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
+
+
+            }
+
+
+            Divider()
+            .padding(/*@START_MENU_TOKEN@*/.horizontal/*@END_MENU_TOKEN@*/)
+
+
+//>>>>>>> 8d637eb231977b632cc5e55feb18cb9de61b328d
+//            Spacer()
+//
+//            AMLMapView(mapState: mapState)
+//                .showUserLocation(true)
+//                .edgesIgnoringSafeArea(.all)
+//
+//
+//            Spacer()
+//            Text("Tracking view")
+//            Spacer()
+//
+//            Button("Sign Out", action: {
+//                sessionManager.signOut()
+//            })
+//        }
+//<<<<<<< HEAD
+//        .onAppear(perform: getRunnerLocation)
+//    }
+//
+//    // MARK: deleteThis
+//    // User's Coordinates:  CLLocationCoordinate2D(latitude: 35.7020691, longitude: 139.7753269)
+//    // locationManager - got locations: [<+35.70206910,+139.77532690> +/- 5.00m (speed -1.00 mps / course -1.00) @ 2/9/22, 1:27:34 AM Eastern Standard Time]
+//
+//    func getRunnerLocation() {
+//
+//        locationService.storeUsername(id: "Database")
+//
+//        locationService.coordinatesPublisher
+//            .receive(on: DispatchQueue.main)
+//            .sink { coordinates in
+//                print("getCurrentUserLocation - user's Coordinates: ", coordinates)
+//                self.mapState.center = coordinates
+//            }
+//            .store(in: &tokens)
+//
+//=======
+        }
+        .background(CusColor.backcolor.edgesIgnoringSafeArea(.all))
+        .onAppear(perform: {
+            if sessionManager.databaseManager.emergencyContacts.isEmpty {
+                if sessionManager.databaseManager.currentUser == nil {
+                    print("Error, current user empty")
+                    sessionManager.showSession()
+                } else {
+                    sessionManager.databaseManager.getEmergencyContacts()
+                    print("This is the emergency contacs: \(sessionManager.databaseManager.emergencyContacts)")
+                }
+            }
+        })
+
+//>>>>>>> 8d637eb231977b632cc5e55feb18cb9de61b328d
     }
     
     
