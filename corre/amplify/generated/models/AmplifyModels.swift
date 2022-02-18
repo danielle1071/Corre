b51f78ba967db3712d82325c19667ec1434d7d15
Lucas <lucas.morehouse@hotmail.com>
2022-02-17 19:05:46 -0500

    updating table data models

--- corre/amplify/generated/models/AmplifyModels.swift
+++ corre/amplify/generated/models/AmplifyModels.swift
@@ -5,7 +5,7 @@ import Foundation
 // Contains the set of classes that conforms to the `Model` protocol. 
 
 final public class AmplifyModels: AmplifyModelRegistration {
-  public let version: String = "a187b2fd5f42f6cab92580c17f8f1d13"
+  public let version: String = "4478a0c3a8297f254c193d5f0bc373bc"
   
   public func registerModels(registry: ModelRegistry.Type) {
     ModelRegistry.register(modelType: Message.self)
