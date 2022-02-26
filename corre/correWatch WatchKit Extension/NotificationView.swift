74b2e16b123a361b259cce0c141cb3f2290e308d
Lauren Wright <laurennnwrighttt@gmail.com>
2022-02-26 18:25:18 -0500

    Apple Watch project added NO UI SIMPLE HELLO WORLD VIEW

---
+++ corre/correWatch WatchKit Extension/NotificationView.swift
@@ -0,0 +1,20 @@
+//
+//  NotificationView.swift
+//  correWatch WatchKit Extension
+//
+//  Created by Lauren Wright on 2/26/22.
+//
+
+import SwiftUI
+
+struct NotificationView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct NotificationView_Previews: PreviewProvider {
+    static var previews: some View {
+        NotificationView()
+    }
+}
