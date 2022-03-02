8a087e1f101a82cca33157133d27e18e3723dc07
danielle1071 <n.danielle1071@gmail.com>
2022-03-02 14:08:02 -0500

    added apple watch pages and notification UI

---
+++ corre/correWatch WatchKit Extension/DashboardView.swift
@@ -0,0 +1,65 @@
+//
+//  DashboardView.swift
+//  correWatch WatchKit Extension
+//
+//  Created by Danielle Nau on 3/2/22.
+//
+
+import SwiftUI
+
+struct DashboardView: View {
+    struct CusColor {
+        static let backcolor =
+            Color("backgroundColor")
+        
+        static let primarycolor = Color("primaryColor")
+        
+        static let lblue = Color("lightBlue")
+    }
+    var body: some View {
+        ZStack{
+            CusColor.backcolor
+                         .ignoresSafeArea()
+       
+            VStack{
+                Text(" Welcome Back!")
+                    .foregroundColor(CusColor.primarycolor)
+                Spacer()
+                HStack{
+                Text("Total Runs: 0")
+                        .foregroundColor(CusColor.primarycolor)
+                        .font(.system(size: 15.0))
+                        .padding(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
+                Spacer()
+                
+                }
+                HStack{
+                Text("Total Distance: 0 mi")
+                        .foregroundColor(CusColor.primarycolor)
+                        .font(.system(size: 15.0))
+                        .padding(/*@START_MENU_TOKEN@*/.horizontal/*@END_MENU_TOKEN@*/)
+                        
+                Spacer()
+                
+                }
+
+                Button(action:{
+                }, label: {
+                    Text("Start Run")
+                        .frame(width: 100, height: 40)
+                        .foregroundColor(Color.white)
+                        .background(CusColor.primarycolor)
+                        .clipShape(Capsule())
+                })
+                   
+            Spacer()
+            }
+        }
+    }
+}
+
+struct DashboardView_Previews: PreviewProvider {
+    static var previews: some View {
+        DashboardView()
+    }
+}
