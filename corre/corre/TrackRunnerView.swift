e1cc22c5176b3376d1a2f2667edf37e3c6a99388
Jon Abrigo <jonabrigo@knights.ucf.edu>
2022-03-17 14:01:35 -0400

    test2

--- corre/corre/TrackRunnerView.swift
+++ corre/corre/TrackRunnerView.swift
@@ -14,6 +14,7 @@ import CoreLocation
 
 struct TrackRunnerView: View {
     
+    // test2
     
     @EnvironmentObject var sessionManager: SessionManger
     @StateObject var trackingManager = TrackerManager()
