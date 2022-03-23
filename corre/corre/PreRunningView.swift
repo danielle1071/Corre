25efc192d537e5fd4640ca14c742dd31c16a5de3
Jon Abrigo <jonabrigo@knights.ucf.edu>
2022-03-23 10:18:32 -0400

    quick commit

--- corre/corre/PreRunningView.swift
+++ corre/corre/PreRunningView.swift
@@ -11,8 +11,8 @@ struct PreRunningView: View {
     
     
     @EnvironmentObject var sessionManager: SessionManger
+    var DEBUG = true
 
-    
     struct CusColor {
         static let backcolor =
             Color("backgroundColor")
@@ -63,6 +63,7 @@ struct PreRunningView: View {
                     EmergencyContactRow(emergencyContact: emergencyContact)
                         .onTapGesture(perform: {
                             sessionManager.showRunning(phoneNumber: emergencyContact.phoneNumber)
+                            self.startRunNotif()
                         })
                 }
             }
@@ -71,6 +72,26 @@ struct PreRunningView: View {
             .padding(/*@START_MENU_TOKEN@*/.horizontal/*@END_MENU_TOKEN@*/)
         }
         .background(CusColor.backcolor.edgesIgnoringSafeArea(.all))
+    }
+    
+    func startRunNotif() {
+        sessionManager.databaseManager.getEmergencyContacts()
+        let contacts = sessionManager.databaseManager.emergencyContacts
+        
+        contacts.forEach { contact in contacts
+            if (DEBUG) {
+                print("PreRunningView -> Emergency Contact: \(contact.id) and \(contact.emergencyContactAppUsername)")
+            }
+            
+            // In the event that the emergency has an account through corre
+            if (contact.emergencyContactAppUsername != nil) {
+                print("DO THE THANG")
+            }
+            
+            // 1EBB9239-8A8B-4326-B8AB-436268DBA895
+            
+        }
+        
     }
 }
 
